46a8d8bffc75f2e24a2452c7ad5b866ddcf09910
SamBS1 <beksultanov12@gmail.com>
2023-02-23 18:49:35 +0600

    first commit

---
+++ src/MainRoutes.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Routes, Route} from 'react-router-dom'
+import Bg from "./components/Bg";
+
+
+const MainRoutes = () => {
+    const PUBLIC_ROUTES = [
+        {
+            link: '/asd',
+            element: <Bg/>,
+            id: 1
+        }
+    ]
+
+return (
+    <Routes>
+        {PUBLIC_ROUTES.map(item => (
+            <Route path={item.link} element={item.element} key={item.id}/>
+        ))}
+
+    </Routes>
+  )
+}
+
+
+export default MainRoutes
---
+++ src/components/Bg.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Parallax, ParallaxLayer} from '@react-spring/parallax'
+ 
+const Bg = () => {
+  return (
+    <>
+    <Parallax>
+
+    <ParallaxLayer >
+jhdsgjgjslg
+    </ParallaxLayer>
+
+    </Parallax>
+    </>
+  )
+}
+
+export default Bg
